002cf4f8eb6a3bfa33022300d4b368d674785df2
Tarik Boukaibat <96335226+TarikBoukaibat@users.noreply.github.com>
2022-03-25 18:06:32 +0100

    Update views.py

--- qr_code_generator/qr_code_generator/views.py
+++ qr_code_generator/qr_code_generator/views.py
@@ -9,14 +9,10 @@ def home(request):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            #box_size=10,
-           # border=1,
         )
         qr.add_data(data)
         qr.make(fit=True)
-
         img = qr.make_image(fill_color="black", back_color="white")
-        #img=make(data)
         img.save("static/image/QR Code.png")
     else:
         pass
